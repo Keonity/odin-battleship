023266d5bce6565383124312daa80212c496697b
Keonity <56704285+Keonity@users.noreply.github.com>
2024-12-09 15:52:47 -0800

    Added receive attack function to gameboard.

--- src/gameboard.js
+++ src/gameboard.js
@@ -4,21 +4,34 @@ class Gameboard {
     constructor(length, width) {
         this.length = length;
         this.width = width;
-        this.board = new Array(length, width);
+        this.board = new Array(length);
+        for (let i = 0; i < length; i++) {
+            this.board[i] = new Array(width);
+        }
     }
 
     placeShip(x, y) {
         const newShip = new Ship(2);
-        this.board[x, y] = newShip;
-        this.board[x, y+1] = newShip;
+        this.board[x][y] = newShip;
+        this.board[x][y + 1] = newShip;
     }
 
     getShip(x, y) {
-        if (this.board[x, y] == null) {
+        if (this.board[x][y] == null || this.board[x][y] == 0) {
             return null;
         }
         else {
-            return this.board[x, y];
+            return this.board[x][y];
+        }
+    }
+
+    receiveAttack(x, y) {
+        if (this.board[x][y] == null || this.board[x][y] == 0) {
+            return null;
+        }
+        else {
+            this.board[x][y].hit();
+            return this.board[x][y].timesHit;
         }
     }
 
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -12,5 +12,23 @@ test('Width 6', () => {
 
 test('Place Ship (3, 3) & Get Ship (3, 3)', () => {
     newBoard.placeShip(3, 3);
-    expect(newBoard.getShip(3, 3)).toBe(newBoard.board[3, 4]);
+    expect(newBoard.getShip(3, 3)).toBe(newBoard.board[3][4]);
+})
+
+test('Receive attack (3, 3)', () => {
+    newBoard.receiveAttack(3, 3);
+    expect(newBoard.getShip(3, 3).timesHit).toEqual(1);
+})
+
+test('Is sunk 1 hit', () => {
+    expect(newBoard.getShip(3, 3).isSunk()).toEqual(false);
+})
+
+test('Receive attack (3, 3)', () => {
+    newBoard.receiveAttack(3, 4);
+    expect(newBoard.getShip(3, 3).timesHit).toEqual(2);
+})
+
+test('Is sunk 2 hits', () => {
+    expect(newBoard.getShip(3, 3).isSunk()).toEqual(true);
 })
