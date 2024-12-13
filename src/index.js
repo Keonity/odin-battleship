97d34075d582ffcfbaf833763abd2b65e1da3504
Keonity <56704285+Keonity@users.noreply.github.com>
2024-12-13 00:44:35 -0800

    Added basic space clicking functionality.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './styles.css';
+
+const playerBoard = document.getElementById("playerBoard");
+const cpuBoard = document.getElementById("cpuBoard");
+
+const playerSpaces = Array.from(playerBoard.children);
+const cpuSpaces = Array.from(cpuBoard.children);
+
+playerSpaces.forEach((child, index) => {
+    child.addEventListener("click", () => {
+        console.log(`Clicked player ${child} #${index}`);
+    })
+})
+
+cpuSpaces.forEach((child, index) => {
+    child.addEventListener("click", () => {
+        console.log(`Clicked cpu ${child} #${index}`);
+    })
+})
